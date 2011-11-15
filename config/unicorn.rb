3c97eed53b9fbe9c02fb0c02e8618df774e02c9e
Michael Guterl <michael@diminishing.org>
2011-11-15 11:00:44 -0500

    Chop down unicorn configuration

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -26,56 +26,6 @@ if GC.respond_to?(:copy_on_write_friendly=)
   GC.copy_on_write_friendly = true
 end
 
-before_fork do |server, worker|
-  ##
-  # When sent a USR2, Unicorn will suffix its pidfile with .oldbin and
-  # immediately start loading up a new version of itself (loaded with a new
-  # version of our app). When this new Unicorn is completely loaded
-  # it will begin spawning workers. The first worker spawned will check to
-  # see if an .oldbin pidfile exists. If so, this means we've just booted up
-  # a new Unicorn and need to tell the old one that it can now die. To do so
-  # we send it a QUIT.
-  #
-  # Using this method we get 0 downtime deploys.
-
-  old_pid = Rails.root.join('tmp/pids/unicorn.pid.oldbin')
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
-end
-
-after_fork do |server, worker|
-  addr = "127.0.0.1:#{9293 + worker.nr}"
-  server.listen(addr, :tries => -1, :delay => 5, :tcp_nopush => true)
-
-  ##
-  # Unicorn master is started as root, which is fine, but let's
-  # drop the workers to deploy:deploy
-
-  begin
-    uid, gid = Process.euid, Process.egid
-    user, group = 'deploy', 'deploy'
-    target_uid = Etc.getpwnam(user).uid
-    target_gid = Etc.getgrnam(group).gid
-    worker.tmp.chown(target_uid, target_gid)
-    if uid != target_uid || gid != target_gid
-      Process.initgroups(user, target_gid)
-      Process::GID.change_privilege(target_gid)
-      Process::UID.change_privilege(target_uid)
-    end
-  rescue => e
-    if rails_env == 'development'
-      STDERR.puts "couldn't change user, oh well"
-    else
-      raise e
-    end
-  end
-end
-
 before_exec do |server|
   paths = (ENV["PATH"] || "").split(File::PATH_SEPARATOR)
   paths.unshift "#{shared_bundler_gems_path}/bin"
