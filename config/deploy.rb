d9a864d27934acce2d7d15d1f8ce9469aeb7b869
Michael Guterl <michael@diminishing.org>
2011-11-15 11:01:12 -0500

    Update capistrano config to use unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 set :stages, %w(staging production)
 set :default_stage, "staging"
 require 'capistrano/ext/multistage'
+require 'bundler/capistrano'
 
 set :application, "search-and-employ"
 set :repository,  "git://github.com/recruitmilitary/nesta.git"
@@ -16,24 +17,36 @@ set :scm, :git
 set :deploy_via, :remote_cache
 set :git_enable_submodules, true
 
-role :web, "web1.recruitmilitary.com"
-role :app, "web1.recruitmilitary.com"
-role :db,  "web1.recruitmilitary.com", :primary => true
+role :web, "web3.recruitmilitary.com"
+role :app, "web3.recruitmilitary.com"
+role :db,  "web3.recruitmilitary.com", :primary => true
 
 ssh_options[:keys] = [File.expand_path("~/.ssh/id_dsa"), File.expand_path("~/.ssh/id_rsa")]
 ssh_options[:forward_agent] = true
 
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
+  task :start, :roles => :app, :except => { :no_release => true } do
+    run "/etc/init.d/search-and-employ start"
+  end
+
+  task :stop, :roles => :app, :except => { :no_release => true } do
+    run "/etc/init.d/search-and-employ stop"
+  end
+
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    run "/etc/init.d/search-and-employ upgrade"
   end
 
   desc "Symlinks the database.yml"
   task :symlink_config, :roles => :app do
     run "ln -nfs #{deploy_to}/shared/config/config.yml #{release_path}/config/config.yml"
   end
+
+  desc "symlink sockets"
+  task :symlink_sockets, :except => { :no_release => true } do
+    run "ln -nfs #{shared_path}/sockets #{latest_release}/tmp/sockets"
+  end
 end
 
 after 'deploy:update_code', 'deploy:symlink_config'
+after "deploy:update_code", "deploy:symlink_sockets"
